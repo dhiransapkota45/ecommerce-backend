9cae12dc5ffcc10523d3a03fb240024544f4fe43
dhiransapkota45 <dhiransapkota55@gmail.com>
2022-12-26 19:38:18 +0545

    database url c

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const cors = require("cors");
 const user = require("./routes/userRoute");
 const product = require("./routes/productRoute");
 const cart = require("./routes/cartRoute");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
-
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -15,14 +14,9 @@ app.use("/", user);
 app.use("/", product);
 app.use("/", cart);
 
-
-// app.get("/hello", (req, res)=>{
-//   console.log(req.query);
-// })
-
 app.listen(process.env.PORT, () => {
   mongoose
-    .connect("mongodb://localhost:27017/ecommerce")
+    .connect(process.env.MONGODBURL)
     .then(() => {
       console.log("connected to the database");
     })
